f50d483daaf1baf96e5426a66b27222b59ba58ea
nico_floriani <floriani.nico@gmail.com>
2023-06-25 10:43:49 -0700

    Created functions to fetch repo data and display information about each repo

--- js/script.js
+++ js/script.js
@@ -2,7 +2,10 @@
 const overview = document.querySelector(".overview");
 //Github username
 const username = "nicofloriani";
+//Repo list
+const repoList = document.querySelector(".repo-list");
 
+//Fetch user data
 const githubUserInfo = async function () {
     const userInfo = await fetch(`https://api.github.com/users/${username}`);
     const data = await userInfo.json();
@@ -28,4 +31,22 @@ const displayUserInfo = function (data) {
     </div> 
     `;
     overview.append(div);
+    gitRepos();
+};
+
+//Fetch repos
+const gitRepos = async function () {
+    const getRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await getRepos.json();
+    displayRepos(repoData);
+};
+
+//Display info about repos
+const displayRepos = function (repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+    }
 };
